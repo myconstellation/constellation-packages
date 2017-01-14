215356b88ddf0bbd4838908a91631f12a85d14aa
Alpha <Alpha@MIDGARD>
2017-01-14 12:52:39 +0100

    PanasonicTV release

---
+++ PanasonicTV/PanasonicTV/Remote/Interfaces/IRemoteController.cs
@@ -0,0 +1,22 @@
+﻿namespace PanasonicTV.Remote.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Interface 
+    /// Show a basic remote controller struct
+    /// </summary>
+    public interface IRemoteController<T,String>
+    {
+        /// <summary>
+        /// Send a key to the api
+        /// </summary>
+        /// <param name="command"> Command explanation </param>
+        /// <param name="target"> Command target </param>
+        void SendKey(T command, string target);
+    }
+}
