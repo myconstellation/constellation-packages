d7428902d7e5a7ad4f5fccee11aed205df72b57e
Alpha <Alpha@MIDGARD>
2017-01-15 09:55:11 +0100

    Squeezebox release

---
+++ Squeezebox/README.md
@@ -0,0 +1,79 @@
+# Squeezebox Package for Constellation
+
+Package to control Squeezebox (Logitech Media Server).
+
+### MessageCallbacks SendToServer
+ - Mute_Toggle : Toggle muting for all the players
+ - Mute_Off : Disable muting for all the players
+ - Mute_On : Mute all the players
+ - Pause : Pause all the players
+ - Play : Play all the players
+ - Play_Toggle : Toggle play and pause state for all the players
+ - Power_Off : Power off all the players
+ - Power_On : Power On all the players
+ - Power_Toggle : Toggle power for all the players
+ - Scan_Cancel : Cancel the actual scan
+ - Scan_Fast : Launch fast scan
+ - Scan_Full : Launch full scan
+ - Stop : Stop all the players
+ - Sync_Off : Disable syncing for all the players
+ - Sync_To : Sync all the players to a target player (require target player name)
+ - Volume : Set the volume of all the players (require volume level)
+ - Volume_Down : Decrease the volume of all the players
+ - Volume_Up : Increase the volume of all the players
+
+### MessageCallbacks SendToSqueezebox
+ - Mute_Off : Disable muting
+ - Mute_On : Enable muting
+ - Mute_Toggle : Toggle muting
+ - Next : Launch the next song
+ - Pause : Pause music
+ - Play : Play music
+ - Play_Album : Launch an album (require album name)
+ - Play_Artist : Launch an artist (require artist name)
+ - Play_Playlist : Launch a playlist (require playlist name)
+ - Play_Title : Launch a title (require title)
+ - Play_Toggle : Toggle pause state
+ - Playlist_Clear : Erase current playlist
+ - Power_Off : Power Off
+ - Power_On : Power On
+ - Power_Toggle : Toggle power state
+ - Previous : Launch the previous song
+ - Random_Album : Launch a random play by album
+ - Random_Artist : Launch a random play by artist
+ - Random_Title : Launch a random play by title
+ - Random_Year : Launch a random play by year
+ - Repeat_Off : Disable repeat
+ - Repeat_Playlist : Repeat playlist
+ - Repeat_Title : Repeat title
+ - Repeat_Toggle : Toggle repeat state
+ - Shuffle_Album : Shuffle by album
+ - Shuffle_Off : Shuffle Off
+ - Shuffle_Title : Shuffle by title
+ - Shuffle_Toggle : Toggle shuffle state
+ - Stop : Stop music
+ - Sync : Sync another player to this player (require target player)
+ - Sync_Off : Disable syncing
+ - Sync_To : Sync this player to another player (require target player)
+ - Volume : Set volume (require volume level)
+ - Volume_Down : Decrease volume
+ - Volume_Up : Increase volume
+
+### Installation
+
+Declare the package in a Sentinel with the following configuration :
+```xml
+	<package name="Squeezebox" enable="true">
+		<settings>
+			<setting key="ServerUrl" value="IP Adress:Port" />
+		</settings>
+	</package>
+```
+
+### Settings
+ - ServerUrl (string - required) : IP adress of the Logitech Media Server with port (for exemple : 192.168.0.50:9000)
+
+License
+----
+
+Apache License
---
+++ Squeezebox/Squeezebox/Remote/HttpServerController.cs
@@ -0,0 +1,110 @@
+﻿namespace Squeezebox.Remote
+{
+    using Squeezebox.Remote.Attributes;
+    using Squeezebox.Remote.Enumerations;
+    using Squeezebox.Remote.Interfaces;
+    using Constellation;
+    using Constellation.Package;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using System.IO;
+
+    public class HttpServerController : IServerController<ServerCommand, string>, IDisposable
+    {
+        public string RemoteKey { get; set; }
+        public WebClient HttpClient { get; set; }
+        const string HttpApiUrlTemplateServer = "{{\"id\":1,\"method\":\"slim.request\",\"params\":[\"\",[{0}]]}}";
+        const string HttpApiUrlTemplateSqueezebox = "{{\"id\":1,\"method\":\"slim.request\",\"params\":[\"{0}\",[{1}]]}}";
+
+        public HttpServerController(string remoteKey)
+        {
+            if (string.IsNullOrEmpty(remoteKey))
+                throw new InvalidOperationException("Remote Key must be provided");
+            this.RemoteKey = remoteKey;
+            this.HttpClient = new WebClient();
+        }
+
+        public void SendKey(ServerCommand command, string value)
+        {
+            string url = "http://" + this.RemoteKey + "/jsonrpc.js";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            string command_name = command.ToString();
+            if (command_name != "Scan_Fast")
+            {
+                PackageHost.StateObjectUpdated += (s, e) =>
+                {
+                    foreach (dynamic hw in e.StateObject.DynamicValue)
+                    {
+                        System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+                        Byte[] byteArray = encoding.GetBytes(this.GenerateCommandFromUrlToSqueezebox(command, hw.name));
+                        request.ContentLength = byteArray.Length;
+                        request.ContentType = @"application/json";
+                        PackageHost.WriteInfo("{0}", this.GenerateCommandFromUrlToSqueezebox(command, hw.name));
+                        using (Stream dataStream = request.GetRequestStream())
+                        {
+                            dataStream.Write(byteArray, 0, byteArray.Length);
+                        }
+                        long length = 0;
+                        try
+                        {
+                            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                            {
+                                length = response.ContentLength;
+                            }
+
+                        }
+                        catch (WebException ex)
+                        {
+                            PackageHost.WriteError(ex);
+                        }
+                    }
+                };
+                PackageHost.RequestStateObjects(sentinel: "Squeezebox", package: "Info", name: "Players");
+            }
+            else
+            {
+                System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+                Byte[] byteArray = encoding.GetBytes(this.GenerateCommandFromUrlToServer(command));
+                request.ContentLength = byteArray.Length;
+                request.ContentType = @"application/json";
+                using (Stream dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                }
+                long length = 0;
+                try
+                {
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    {
+                        length = response.ContentLength;
+                    }
+                }
+                catch (WebException ex)
+                {
+                    PackageHost.WriteError(ex);
+                }
+            }
+        }
+
+        private string GenerateCommandFromUrlToServer(ServerCommand command)
+        {
+            return string.Format(HttpApiUrlTemplateServer, command.To<CommandAttribute>());
+        }
+
+        private string GenerateCommandFromUrlToSqueezebox(ServerCommand command, dynamic squeezebox)
+        {
+            return string.Format(HttpApiUrlTemplateSqueezebox, squeezebox, command.To<CommandAttribute>());
+        }
+
+        public void Dispose()
+        {
+            this.HttpClient.Dispose();
+        }
+    }
+}
