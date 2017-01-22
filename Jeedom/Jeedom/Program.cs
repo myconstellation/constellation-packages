90c5b38631b7954c70999139da2717e20469537f
Alpha <Alpha@MIDGARD>
2017-01-22 12:34:37 +0100

    Jeedom release

---
+++ Jeedom/Jeedom/Program.cs
@@ -0,0 +1,94 @@
+﻿using Constellation;
+using Constellation.Package;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Jeedom.Remote.Enumerations;
+using Jeedom.Remote.Interfaces;
+using Jeedom.Remote;
+
+namespace Jeedom
+{
+    public class Program : PackageBase
+    {
+        /// <summary>
+        ///  Main remote controller
+        /// </summary>
+        public IController<int, Command, string> Controller { get; set; }
+
+        /// <summary>
+        ///  Main server controller
+        /// </summary>
+        public ISceneController<int, SceneCommand> SceneController { get; set; }
+
+        /// <summary>
+        /// Remote key config value key
+        /// </summary>
+        public const string JeedomUrl = "ServerUrl";
+
+        /// <summary>
+        /// Remote key config value key
+        /// </summary>
+        public const string JeedomApiKey = "ApiKey";
+
+        /// <summary>
+        /// Free API Remote key
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Free API Remote key
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        static void Main(string[] args)
+        {
+            PackageHost.Start<Program>(args);
+        }
+
+        public override void OnStart()
+        {
+            PackageHost.WriteInfo("Package starting - IsRunning: {0} - IsConnected: {1}", PackageHost.IsRunning, PackageHost.IsConnected);
+
+            //// Getting configuration
+            this.Url = PackageHost.GetSettingValue<string>(JeedomUrl);
+
+            //// Getting configuration
+            this.ApiKey = PackageHost.GetSettingValue<string>(JeedomApiKey);
+
+            //// Remote controller
+            this.Controller = new HttpController(this.Url, this.ApiKey);
+
+            //// Remote controller
+            this.SceneController = new HttpSceneController(this.Url, this.ApiKey);
+
+        }
+
+        /// <summary>
+        /// Send a command to an ID.
+        /// </summary>
+        /// <param name="id">The command ID.</param>
+        /// <param name="command">The command type.</param>
+        /// <param name="value">The command value if necessary.</param>+
+        /// <param name="value2">The command value if necessary.</param>
+        [MessageCallback]
+        public void SendCommand(int id, Command command, string value = null, string value2 = null)
+        {
+            this.Controller.SendKey(id, command, value, value2);
+        }
+
+        /// <summary>
+        /// Control a scene by ID.
+        /// </summary>
+        /// <param name="scene_id">The scene ID.</param>
+        /// <param name="scene_command">The scene command.</param>
+        [MessageCallback]
+        public void SceneControl(int scene_id, SceneCommand scene_command)
+        {
+            this.SceneController.SendKey(scene_id, scene_command);
+        }
+
+    }
+}
---
+++ Jeedom/Jeedom/Remote/HttpController.cs
@@ -0,0 +1,70 @@
+﻿namespace Jeedom.Remote
+{
+    using Jeedom.Remote.Attributes;
+    using Jeedom.Remote.Enumerations;
+    using Jeedom.Remote.Interfaces;
+    using Constellation;
+    using Constellation.Package;
+    using System;
+    using System.Net;
+    using System.IO;
+
+    public class HttpController : IController<int, Command, string>, IDisposable
+    {
+        public string Url { get; set; }
+        public string ApiKey { get; set; }
+        public WebClient HttpClient { get; set; }
+        const string HttpApiJeedom = "http://{0}/core/api/jeeApi.php?apikey={1}&type=cmd{2}";
+
+        public HttpController(string JeedomUrl, string JeedomApiKey)
+        {
+            if (string.IsNullOrEmpty(JeedomUrl))
+                throw new InvalidOperationException("Jeedom Url must be provided");
+            if (string.IsNullOrEmpty(JeedomApiKey))
+                throw new InvalidOperationException("Jeedom Api Key must be provided");
+            this.Url = JeedomUrl;
+            this.ApiKey = JeedomApiKey;
+            this.HttpClient = new WebClient();
+        }
+
+        public void SendKey(int id, Command command, string value, string value2)
+        {
+            string url = this.GenerateUrl(id, command, value, value2);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            System.Text.UTF8Encoding encoding = new System.Text.UTF8Encoding();
+            Byte[] byteArray = encoding.GetBytes(url);
+            request.ContentLength = byteArray.Length;
+            request.ContentType = @"application/json";
+            using (Stream dataStream = request.GetRequestStream())
+            {
+                dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+            long length = 0;
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    length = response.ContentLength;
+                    PackageHost.WriteInfo("Command send to Jeedom");
+                }
+            }
+            catch (WebException ex)
+            {
+                PackageHost.WriteError(ex);
+            }
+        }
+
+        public string GenerateUrl(int id, Command command, string value, string value2)
+        {
+            string commandhttp =  string.Format(command.To<CommandAttribute>(), id, value, value2);
+            return string.Format(HttpApiJeedom, Url, ApiKey, commandhttp);
+
+        }
+
+        public void Dispose()
+        {
+            this.HttpClient.Dispose();
+        }
+    }
+}
