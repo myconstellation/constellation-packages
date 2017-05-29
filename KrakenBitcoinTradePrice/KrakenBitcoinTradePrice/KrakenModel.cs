0b0f3ebed3311afb0dca4df9d64e3223cbb4eafe
Cllio <clement.lion@gmail.com>
2017-05-29 10:20:49 +0200

    Get Bitcoin trade price in EUR of https://www.kraken.com/ and pushes it to Constellation

---
+++ KrakenBitcoinTradePrice/KrakenBitcoinTradePrice/KrakenModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrakenBitcoinTradePrice
+{
+    public class KrakenRootObject
+    {
+        public KrakenResult result { get; set; }
+    }
+
+    public class KrakenResult
+    {
+        public Bitcoin XXBTZEUR { get; set; }
+    }
+
+    public class Bitcoin
+    {
+        public List<string> c { get; set; }
+    }
+}
---
+++ KrakenBitcoinTradePrice/KrakenBitcoinTradePrice/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kraken Bitcoin Trade Price")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cllio Kraken Bitcoin Trade Price")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c910cbd5-2c01-4daa-8099-b4036f05a86d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
