d73b8cc9ec64c2b92765d642bbea8d072531e18e
Erwann Laville <e.laville@protonmail.com>
2017-07-06 17:28:33 +0200

    VigilanceMeteoFrance release

---
+++ VigilanceMeteoFrance/VigilanceMeteoFrance/Program.cs
@@ -0,0 +1,22 @@
+﻿using Constellation;
+using Constellation.Package;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VigilanceMeteoFrance
+{
+    public class Program : PackageBase
+    {
+        static void Main(string[] args)
+        {
+            PackageHost.Start<Program>(args);
+        }
+
+        public override void OnStart()
+        {
+            PackageHost.WriteInfo("Package starting - IsRunning: {0} - IsConnected: {1}", PackageHost.IsRunning, PackageHost.IsConnected);
+        }
+    }
+}
