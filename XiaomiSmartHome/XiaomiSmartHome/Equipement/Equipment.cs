7081bd16eeb620de06c70a27a44d7aef172e553c
Sébastien <sebastien.wilhelm@gmail.com>
2018-06-01 10:55:26 +0200

    Update xiaomi package

---
+++ XiaomiSmartHome/XiaomiSmartHome/Equipement/Equipment.cs
@@ -0,0 +1,70 @@
+﻿using Constellation.Package;
+using LiteDB;
+using Newtonsoft.Json;
+using System;
+using static XiaomiSmartHome.Enums;
+
+namespace XiaomiSmartHome.Equipement
+{
+    /// <summary>
+    /// Base equipment class
+    /// </summary>
+    [StateObject]
+    public class Equipment
+    {
+        /// <summary>
+        /// Model type.
+        /// </summary>
+        public EquipmentType Model { get; set; }
+
+        /// <summary>
+        /// SID (mac adress).
+        /// </summary>
+        public string Sid { get; set; }
+
+        /// <summary>
+        /// Short id
+        /// </summary>
+        [JsonProperty("short_id")]
+        public int ShortId { get; set; }
+
+        /// <summary>
+        /// Voltage left
+        /// </summary>
+        public int Voltage { get; set; }
+
+        /// <summary>
+        /// Battery type.
+        /// </summary>
+        public BatteryType Battery { get; set; }
+
+        /// <summary>
+        /// Battery level
+        /// </summary>
+        public int BatteryLevel { get; set; }
+
+        /// <summary>
+        /// Sensor state
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Update equipment with last data
+        /// </summary>
+        public virtual void Update(object data) { }
+
+        /// <summary>
+        /// Get percent battery left
+        /// </summary>
+        /// <param name="voltage"></param>
+        /// <returns></returns>
+        internal int ParseVoltage(int voltage)
+        {
+            int maxVolt = 3000;
+            int minVolt = 2500;
+            if (voltage > maxVolt) voltage = maxVolt;
+            if (voltage < minVolt) voltage = minVolt;
+            return (int)Math.Round((((decimal)(voltage - minVolt) / (decimal)(maxVolt - minVolt)) * 100));
+        }
+    }
+}
---
+++ XiaomiSmartHome/XiaomiSmartHome/Equipement/MagnetAq2.cs
@@ -0,0 +1,20 @@
+﻿using Constellation;
+using Constellation.Package;
+
+namespace XiaomiSmartHome.Equipement
+{
+    /// <summary>
+    /// Windows door sensor v2
+    /// </summary>
+    [StateObject]
+    public class MagnetAq2 : Magnet
+    {
+        /// <summary>
+        /// Update equipment with last data
+        /// </summary>
+        public override void Update(object data)
+        {
+            base.Update(data);
+        }
+    }
+}
