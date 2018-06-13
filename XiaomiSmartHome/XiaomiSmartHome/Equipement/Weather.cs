549ef67b8354943a551e8985dd218e790a9c7053
Sébastien <sebastien.wilhelm@gmail.com>
2018-06-13 09:09:29 +0200

    Update xiaomi package
    Add equipments

---
+++ XiaomiSmartHome/XiaomiSmartHome/Equipement/Weather.cs
@@ -0,0 +1,64 @@
+﻿using Constellation;
+using Constellation.Package;
+using static XiaomiSmartHome.Enums;
+
+namespace XiaomiSmartHome.Equipement
+{
+    /// <summary>
+    /// Capteur température et humidité
+    /// </summary>
+    [StateObject]
+    public class Weather : Equipment
+    {
+        /// <summary>
+        /// Temperature
+        /// </summary>
+        public int? Temperature { get; set; }
+
+        /// <summary>
+        /// Humidity
+        /// </summary>
+        public int? Humidity { get; set; }
+
+        /// <summary>
+        /// Pressure
+        /// </summary>
+        public int? Pressure { get; set; }
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public Weather()
+        {
+            base.Battery = BatteryType.CR1632;
+        }
+
+        /// <summary>
+        /// Update equipment with last data
+        /// </summary>
+        public override void Update(object data)
+        {
+            Weather curData = data as Weather;
+            if (curData.Voltage != default(int))
+            {
+                this.Voltage = curData.Voltage;
+                this.BatteryLevel = base.ParseVoltage(curData.Voltage);
+            }
+
+            if (curData.Temperature.HasValue)
+            {
+                this.Temperature = curData.Temperature;
+            }
+
+            if (curData.Humidity.HasValue)
+            {
+                this.Humidity = curData.Humidity;
+            }
+
+            if (curData.Pressure.HasValue)
+            {
+                this.Pressure = curData.Pressure;
+            }
+        }
+    }
+}
